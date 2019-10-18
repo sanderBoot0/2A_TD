94eb3f9ef03194ee86f07c1d9c45d1419a00202c
Scott Timmermans <scott.timmermans@gmail.com>
2019-10-18 13:41:30 +0200

    Co-authored-by: sanderBoot0 <sander.boot.0@outlook.com> Co-authored-by: Tobias van den Hoogen <tobias.vandenhoogen@student.hu.nl>

--- opdracht/sendRecieve/source/send_classes.cpp
+++ opdracht/sendRecieve/source/send_classes.cpp
@@ -1,21 +1,12 @@
 #include "../headers/send_classes.hpp"
 
-void ir_sender::send_one() {
-    send_pin.write(1);
-    send_pin.flush();
-    hwlib::wait_us(1600);
-    send_pin.write(0);
-    send_pin.flush();
+void ir_sender::send_bit(const bool &bit) {
+    send_pin.write(1);      send_pin.flush();
     hwlib::wait_us(800);
-}
-
-void ir_sender::send_zero() {
-    send_pin.write(1);
-    send_pin.flush();
+    send_pin.write(bit);    send_pin.flush();
     hwlib::wait_us(800);
-    send_pin.write(0);
-    send_pin.flush();
-    hwlib::wait_us(1600);
+    send_pin.write(0);      send_pin.flush();
+    hwlib::wait_us(800);
 }
 
 void ir_sender::set_zero() { send_pin.write(0); }
@@ -23,11 +14,12 @@ void ir_sender::set_zero() { send_pin.write(0); }
 void ir_sender::send_message(char16_t compiled_message) {
         send_one();
         for (int i = 15; i >= 0; i--) {
-            if ((compiled_message & (1 << i)) != 0) {
-                send_one();
-            } else {
-                send_zero();
-            }
+            // if ((compiled_message & (1 << i)) != 0) {
+            //     send_one();
+            // } else {
+            //     send_zero();
+            // }
+            send_bit( ( (compiled_message & (1 << i)) != 0) ? 1 : 0 );
         }
         send_pin.write(0);
         send_pin.flush();
